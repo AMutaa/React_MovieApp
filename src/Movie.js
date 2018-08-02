3ce1a1a28bd5f178d96eff7ab3f7d3c153c09523
AMutaa <piusandru@gmail.com>
2018-08-02 16:53:18 -0400

    API fetch

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Movie extends Component {
+  static propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      desc: PropTypes.string
+    })
+  }
+
   render() {
     return (
       <div>
-        {this.props.movie.title}
+        <h3>{this.props.movie.title}</h3>
+
+
       </div>
     )
   }
