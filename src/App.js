d5ba2a59c234a5e576af8f3c8798bad025b21a5c
AMutaa <piusandru@gmail.com>
2018-08-02 15:26:16 -0400

    first components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+
+const movies = [{
+  id: 1,
+  title: 'Star Wars'
+}, {
+  id: 2,
+  title: 'Spider Man'
+}, {
+  id: 3,
+  title: 'Blacksakan'
+}
+]
 
 class App extends Component {
   render() {
@@ -8,14 +21,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+
       </div>
     );
   }
 }
 
-]
+export default App;
