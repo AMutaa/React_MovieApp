23298f6dac240cbcf79d0caa9adc693bb8284cf8
AMutaa <piusandru@gmail.com>
2018-08-03 20:33:35 -0400

    styled components

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+import { Poster } from './Movie';
 
-
-const POSTER_PATH = 'http://image.tmdb.org/t/p/w1280';
-
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
 
 class MovieDetail extends Component {
   state = {
@@ -11,6 +12,7 @@ class MovieDetail extends Component {
   async componentDidMount() {
     try {
       const res = await fetch(`https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=131c87cb54773ae0f6c9b813ce9041b2&language=en-US`);
+
       const movie = await res.json();
       this.setState({
         movie: movie,
@@ -26,16 +28,38 @@ class MovieDetail extends Component {
     const { movie } = this.state;
 
     return (
-      <div>
-        <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
-        <h1>{movie.title}</h1>
-        <h3>{movie.release_date}</h3>
-        <p>{movie.overview}</p>
-      </div>
-
+      <MovieWrapper backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
+        <MovieInfo>
+          <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+          <div>
+            <h1>{movie.title}</h1>
+            <h3>{movie.release_date}</h3>
+            <p>{movie.overview}</p>
+          </div>
+        </MovieInfo>
+      </MovieWrapper>
     );
   }
 }
 
 export default MovieDetail;
 
+const MovieWrapper = styled.div`
+ position: relative;
+ padding-top:50vh;
+
+ background-size:cover;
+`;
+
+const MovieInfo = styled.div`
+background:white;
+text-align:left;
+padding: 2rem 10%;
+display: flex;
+>div{
+  margin-left:20px;
+}
+img {
+  position:relative;
+}
+`;
