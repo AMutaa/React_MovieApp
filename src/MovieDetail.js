d66e5cde2e165ea929a67b43ee19254cf84491d7
AMutaa <piusandru@gmail.com>
2018-10-18 12:30:28 -0400

    update

--- src/MovieDetail.js
+++ src/MovieDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Overdrive from 'react-overdrive';
+import styled from 'styled-components'
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 const BACKDROP_PATH = 'http://image.tmdb.org/t/p/w1280';
 
@@ -29,7 +30,7 @@ class MovieDetail extends Component {
         return (
             <div backdrop={`${BACKDROP_PATH}${movie.backdrop_path}`}>
                 <img className="backdrop" src={`${BACKDROP_PATH}${movie.backdrop_path}`} alt={movie.title} />
-                <mOVIE>
+                <MovieInfo>
                     <Overdrive id={movie.id}>
                         <img className="poster" src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
                     </Overdrive>
@@ -38,10 +39,20 @@ class MovieDetail extends Component {
                         <h3>{movie.release_date}</h3>
                         <p>{movie.overview}</p>
                     </div>
-                </div >
+                </MovieInfo >
             </div >
         );
     }
 }
 
 export default MovieDetail;
+
+const MovieInfo = styled.div`
+background: white;
+text-align: left;
+padding: 2rem 2rem;
+display: flex;
+border: 2px solid #0F2027;
+border-radius: 7px;
+margin: 1rem 20%
+`;
